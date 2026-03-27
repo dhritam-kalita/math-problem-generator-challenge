8f234c7559ee74337ec50eac41c6866932429ae3
dhritam-kalita <dhritam.kalita99@gmail.com>
2026-03-27 11:31:01 +0530

    optimized the answer generation + round up time taken to 2 deciamal places + increased total problems to 10 + refined problem generation logic

--- math-challenge-generator.py
+++ math-challenge-generator.py
@@ -4,20 +4,16 @@ from time import time
 MIN_OPERAND = 3
 MAX_OPERAND = 12
 OPERATORS = ('+', '-', '*')
-TOTAL_PROBLEMS = 3
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     operand_1 = randint(MIN_OPERAND, MAX_OPERAND)
     operand_2 = randint(MIN_OPERAND, MAX_OPERAND)
     operator = choice(OPERATORS)
-    if operator == '+':
-        answer = operand_1 + operand_2
-    elif operator == '-':
-        answer = operand_1 - operand_2
-    else:
-        answer = operand_1 * operand_2
     
-    problem = f"{operand_1} {operator} {operand_2} = "
+    problem = f"{operand_1} {operator} {operand_2}"
+    answer = eval(problem)
+    
     return problem, answer
 
 input("Press ENTER to start: ")
@@ -27,11 +23,11 @@ for i in range(TOTAL_PROBLEMS):
     ques, ans = generate_problem()
     
     while True:
-        user_ans = input(f"Problem #{i + 1}: {ques}")
+        user_ans = input(f"Problem #{i + 1}: {ques} = ")
         if user_ans == str(ans):
             break
 end_time = time()
-time_taken = end_time - start_time
+time_taken = round(end_time - start_time, 2)
 print("--------------------------")
 print(f"Congratulations! You solved all the problems in {time_taken} seconds.")
     
